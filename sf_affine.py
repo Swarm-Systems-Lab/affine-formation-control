81342c193f8e88c94b27586724e464733a445c64
jesusBV20 <rema03@hotmail.es>
2024-03-12 13:47:52 +0100

    Eigenvector in R^2

--- sf_affine.py
+++ sf_affine.py
@@ -88,7 +88,6 @@ class sim_frame_affine:
             with np.printoptions(precision=2, suppress=True):
                 print("W {:s} = \n".format(str(self.W.shape)), self.W)
                 print("L {:s} = \n".format(str(self.L.shape)), self.L)
-                print("bar B^T {:s} = \n".format(str(self.B_bar_T.shape)), self.B_bar_T)
             print(" --------- Eigen values")
             with np.printoptions(precision=8, suppress=True):
                 for i,eigen in enumerate(LA.eig(-self.L)[0]):
@@ -104,114 +103,41 @@ class sim_frame_affine:
     """
     """
     def check_eigen_vectors(self):
-        R45 = rot_transf_from_ang(self.n, np.pi/4)
-        Sc = np.kron(np.eye(self.n), 4*np.eye(self.m))
+        R90 = rot_transf_from_ang(self.n, np.pi/2)
+        Sc = np.kron(np.eye(self.n), np.eye(self.m))
         Shx = sh_transf_x(self.n,1)
         Shy = sh_transf_y(self.n,1)
-        A = np.kron(np.eye(self.n), np.eye(self.m))
-        A[0:2,0:2] = np.array([[1,0],[0,-1]])
+
         with np.printoptions(precision=6, suppress=True):
             print("{:<15} = ".format("L@1_n^bar"), self.L @ np.ones(self.n*self.m))
             print("{:<15} = ".format("L@p^*"), self.L @ self.p_star)
-            print("{:<15} = ".format("L@R(pi/4)@p^*"), self.L @ R45 @ self.p_star)
-            print("{:<15} = ".format("L@(4*I_mn)@p^*"), self.L @ Sc @ self.p_star)
+            print("{:<15} = ".format("L@(I_mn)@p^*"), self.L @ Sc @ self.p_star)
+            print("{:<15} = ".format("L@R(pi/4)@p^*"), self.L @ R90 @ self.p_star)
             print("{:<15} = ".format("L@Shx@p^*"), self.L @ Shx @ self.p_star)
             print("{:<15} = ".format("L@Shy@p^*"), self.L @ Shy @ self.p_star)
             print(" ------------ ")
-            print("{:<15} = ".format("L@1_n^bar"), self.L_mod @ np.ones(self.n*self.m))
-            print("{:<15} = ".format("L@p^*"), self.L_mod @ self.p_star)
-            print("{:<15} = ".format("L@R(pi/4)@p^*"), self.L_mod @ R45 @ self.p_star)
-            print("{:<15} = ".format("L@Shx@p^*"), self.L_mod @ Shx @ self.p_star)
-            print("{:<15} = ".format("L@Shy@p^*"), self.L_mod @ Shy @ self.p_star)
-
-        # test = self.p_star
-        
-        # test = test.reshape(self.n, self.m)
-        # plt.plot(test[:,0], test[:,1], "ok")
-
-
-        # test = Shx @ self.p_star
-        # test = test.reshape(self.n, self.m)
-        # plt.plot(test[:,0], test[:,1], "or", label=r"$\bar S_x \times p^*$")
-
-        # test = Shy @ self.p_star
-        # test = test.reshape(self.n, self.m)
-        # plt.plot(test[:,0], test[:,1], "ob", label=r"$\bar S_y \times p^*$")
-
-        # plt.legend()
-        # plt.grid(True)
-    
-    # """
-    # """
-    # def set_velocity(self, vx, vy, a, omega, sx, sy):
-    #     # Generate the stack velocity vector (n,m)
-    #     S = np.kron(np.eye(self.n), np.eye(self.m))
-    #     R, Sx, Sy, = 0*S, 0*S, 0*S
-    #     if abs(omega)>0:
-    #         R = rot_transf_from_ang(self.n, omega)
-    #     if abs(sx)>0:
-    #         Sx = sh_transf_x(self.n,sx)
-    #     if abs(sy)>0:
-    #         Sy = sh_transf_x(self.n,sy)
-        
-    #     vt = np.kron(np.ones(self.n), [vx,vy])
-    #     vf = vt + (a*S + R + Sx + Sy) @ self.p_star
-    #     vf = vf.reshape(self.n, self.m)
-
-    #     # Generate the matrix of motion marameters mu_ij \in R^m
-    #     ps = self.p_star.reshape(self.n, self.m)
-    #     vf_complex = vf[:,0] + (1j)*vf[:,1]
-    #     ps_complex = ps[:,0] + (1j)*ps[:,1]
-
-    #     mu_matrix = np.zeros((self.n*self.m,self.n*self.m))
-    #     for i in range(self.n):
-    #         j = self.Ni_list[i][0]
-    #         mu_i_complex = vf_complex[i]/(ps_complex[i] - ps_complex[j])
-
-    #         mu_i = np.zeros((self.m,self.n*self.m))
-    #         mu_i[:, j*self.m:(j+1)*self.m] = rot_mat(mu_i_complex)
-    #         mu_matrix[i*self.m:(i+1)*self.m,:] = mu_i
-        
-    #     M = gen_compnts_matrix(self.n, self.m, self.Z, mu_matrix)
-    #     self.L_mod = self.L - self.kappa/self.h * self.K_inv @ M @ self.B_bar_T
-
-    #     if self.debug:
-    #         with np.printoptions(precision=2, suppress=True):
-    #             print("M", M.shape, "B.T", self.B.T.shape, "K_inv", self.K_inv.shape)
-    #             print("mu_ij matrix:\n", mu_matrix)
-    #             print("vf_input:\n", vf)
-    #             print("M^bar@B^T^bar@p_star", M@self.B_bar_T@self.p_star)
             
     """
     """
-    def set_manual_mu(self, mu_matrix, check_eigen_vec = False):
+    def set_manual_mu(self, mu_matrix, debug_eigenvectors=False):
+        mu_matrix = np.kron(mu_matrix, np.eye(self.m))
         self.M = gen_compnts_matrix(self.n, self.m, self.Z, mu_matrix)
         self.L_mod = self.L - self.kappa/self.h * self.K_inv @ self.M @ self.B_bar_T
 
         if self.debug:
             with np.printoptions(precision=2, suppress=True):
                 print("M", self.M.shape, "B.T", self.B.T.shape, "K_inv", self.K_inv.shape)
-                print("mu_ij matrix:\n", mu_matrix)
-                print("M:\n", mu_matrix)
-                print("M^bar@B^T^bar@p_star", self.M@self.B_bar_T@self.p_star)
-            print(" --------- Eigenvalues L_mod")
-            with np.printoptions(precision=8, suppress=True):
-                for i,eigen in enumerate(LA.eig(-self.L_mod)[0]):
-                    print("lambda_{:d} = {:f}".format(i,eigen))
+                #print("mu_ij matrix:\n", mu_matrix)
+                #print("M:\n", mu_matrix)
+                print("M^bar@B^T^bar@p_star:", self.M@self.B_bar_T@self.p_star)
 
-        if check_eigen_vec:
-            R45 = rot_transf_from_ang(self.n, np.pi/4)
-            Shx = sh_transf_x(self.n,1)
-            Shy = sh_transf_y(self.n,1)
+            print(" --------- Eigenvalues L_mod")
             with np.printoptions(precision=4, suppress=True):
-                for i,eigen in enumerate(LA.eig(-self.L_mod)[0]):
-                    L_eig = (-self.L_mod - np.eye(self.n*self.m)*eigen)
-                    # print(" ---- ", eigen)
-                    # print("{:<15} = \n".format("(L - I*lambda)@1_n^bar"), L_eig @ np.ones(self.n*self.m))
-                    # print("{:<15} = \n".format("(L - I*lambda)@p^*"), L_eig @ self.p_star)
-                    # print("{:<15} = \n".format("(L - I*lambda)@R(pi/4)@p^*"), L_eig @ R45 @ self.p_star)
-                    # print("{:<15} = \n".format("(L - I*lambda)@Shx@p^*"), L_eig @ Shx @ self.p_star)
-                    # print("{:<15} = \n".format("(L - I*lambda)@Shy@p^*"), L_eig @ Shy @ self.p_star)
+                eg = LA.eig(-self.h*self.L_mod)
+                for i in range(len(eg[0])):
+                    print("lambda_{:d} = {:f}".format(i,eg[0][i]))
+                    if debug_eigenvectors:
+                        print(eg[1][:,i])
 
     """
     """
@@ -227,10 +153,6 @@ class sim_frame_affine:
             pdata[i,:,:] = self.simulator.p.reshape(self.n,self.m)
             # Robots simulator euler step integration
             self.simulator.int_euler(self.h, self.K, self.L_mod)
-            # Test
-            # u = -self.h * self.K @ self.L @ self.simulator.p.reshape(self.n*self.m,1) + \
-            #      self.kappa * self.K_inv @ self.M @ self.B_bar_T @ self.simulator.p.reshape(self.n*self.m,1)
-            # self.simulator.p = self.simulator.p.reshape(self.n*self.m,1) + u * self.dt
     
         self.data["p"] = pdata
 
